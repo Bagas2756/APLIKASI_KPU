e69941777ab674e3c31ec25c311a92896b28ee7f
Bagas2756 <92680823+Bagas2756@users.noreply.github.com>
2023-12-19 10:27:00 +0700

    frontend

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'react-native-gesture-handler'; 
+import AppNavigator from './src/router';
+
+const App = () => {
+  return (
+    <>
+      <AppNavigator />
+    </>
+  );
+}
+
+export default App
